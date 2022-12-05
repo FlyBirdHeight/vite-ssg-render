e3121dd7c07506a26bb88a7f4eb1ebd0271b30d6
adsionli <adsionli@foxmail.com>
2022-12-05 12:48:36 +0800

    first commit

---
+++ dist/node/cli.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const cac_1 = require("cac");
+const dev_1 = require("./dev");
+const version = require("../../package.json").version;
+const cli = (0, cac_1.cac)("adsionli").version(version).help();
+cli
+    .command("[root]", "start dev server")
+    .alias("dev")
+    .action(async (root) => {
+    const server = await (0, dev_1.createDevServer)(root);
+    await server.listen();
+    server.printUrls();
+});
+cli
+    .command("build [root]", "build for production")
+    .action(async (root) => {
+    console.log("build", root);
+});
+cli.parse();
---
+++ src/node/cli.ts
@@ -0,0 +1,24 @@
+import { cac } from "cac";
+import { createDevServer } from './dev';
+
+const version = require("../../package.json").version;
+
+const cli = cac("adsionli").version(version).help();
+
+cli
+    .command("[root]", "start dev server")
+    .alias("dev")
+    .action(async (root: string) => {
+        const server = await createDevServer(root);
+        await server.listen();
+        server.printUrls();
+
+    })
+
+cli
+    .command("build [root]", "build for production")
+    .action(async (root: string) => {
+        console.log("build", root)
+    });
+
+cli.parse();
