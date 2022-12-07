80733f4d2d3b36478a4d6a77510aac9c9c38dc00
adsionli <adsionli@foxmail.com>
2022-12-07 16:09:53 +0800

    feat: 新增lint-staged

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,31 @@
 module.exports = {
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
-        //eslint自动修复规则
-        'prettier',
-        'plugin:prettier/recommended'
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true
-        },
-        ecmaVersion: 'latest',
-        sourceType: 'module'
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
-    rules: {
-        'prettier/prettier': 'error',
-        quotes: ['error', 'single'],
-        semi: ['error', 'always'],
-        'react/react-in-jsx-scope': 'off'
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/no-unescaped-entities': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect'
     }
-}
+  }
+};
